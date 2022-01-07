59b5a780a493a50c1444676bdf2206dd2230bb13
Jakepps <nagalevsiy@mail.ru>
2022-01-07 21:47:21 +0300

    ???

---
+++ ooooooo))))/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ooooooo____
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
